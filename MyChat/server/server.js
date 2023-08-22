039c9a36313229b41d8c625d8852ed194160d2c9
James Oates <oatsie17@gmail.com>
2023-08-22 21:11:50 +1000

    set up express server

---
+++ MyChat/server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+const http = require('http').Server(app);
+const bodyParser = require('body-parser');
+
+app.use (bodyParser.json());
+
+app.use(express.static(path.join(__dirname, '../dist/my-chat/')))
+
+let server = http.listen(3000, function () {
+    let host = server.address().address;
+    let port = server.address().port;
+    console.log("Sever running!")
+    console.log("Server listenning on: " +host + " port: " + port);
+});
+
+
+
