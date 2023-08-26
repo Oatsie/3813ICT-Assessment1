880d358392098e971bc5a43b3ee230babe1b51fe
James Oates <oatsie17@gmail.com>
2023-08-26 23:37:05 +1000

    UI
    added UI segments

--- MyChat/src/app/app.module.ts
+++ MyChat/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import {HttpClientModule} from '@angular/common/http'
+import { UsersModule } from './Users/users.module';
+import { GroupsComponent } from './groups/groups.component';
+import { ChannelsComponent } from './channels/channels.component';
+import { MessageViewComponent } from './message-view/message-view.component';
+import { MessageCreateComponent } from './message-create/message-create.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, GroupsComponent, ChannelsComponent, MessageViewComponent, MessageCreateComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UsersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ MyChat/src/app/message-view/message-view.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { MessageViewComponent } from './message-view.component';
+
+@NgModule({
+  declarations: [MessageViewComponent],
+  imports: [],
+  exports: [MessageViewComponent],
+})
+export class MessageViewModule {}
